de3fca20632b081d02b16738ccb512d0bed3a541
danimpg <52898709+danimpg@users.noreply.github.com>
2020-06-02 20:34:03 -0600

    Ejercicios_php
    primeros ejercicios

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <body>
+
+      <h1>Ejercicios de PHP</h1>
+      <li><a href="Ejercicio_2.php" target="_new">Ejercicio 2</a></li>
+      <li><a href="Ejercicio_3.php" target="_new">Ejercicio 3</a></li>
+      <li><a href="Ejercicio_4.php" target="_new">Ejercicio 4</a></li>
+      <li><a href="Ejercicio_5.php" target="_new">Ejercicio 5</a></li>
+      <li><a href="" target="_new">Ejercicio 6</a></li>
+      <ul>
+     
+  </body>
+</html>
