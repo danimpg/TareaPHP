566e0d7e2dbdd8e3e8499dd751e9db84cf9531ec
danimpg <52898709+danimpg@users.noreply.github.com>
2020-06-02 22:25:14 -0600

    cambio nuevo
    cambio en ultimo ejercicio

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -7,38 +7,38 @@
       <?php
       header ("Content-type: text/html;charset=\"utf-8\"");
       
-      echo "</h1>Evaluacion Final</h3>";
+      echo "</h1>Numero Ingresado</h3>";
 
       echo "<h1>".$_GET['numero']."</h1>";
 
       if(is_numeric($_GET['numero']))
       {
           echo "<p>Ha ingresado un valor numerico</p>";
-          echo "<br><br>";
+          echo "<br>";
       }
       else{
-        echo "<p>El valor que Ingreso no en numerico</p>";
-        echo "<br><br>";
+        echo "<p>El valor que Ingreso no es un valor numerico</p>";
+        echo "<br>";
       }
       if($_GET['numero'] >= 1)
       {
         echo "<p>El numero ingresado si es mayor que 1</p>";
-        echo "<br><br>";
+        echo "<br>";
       }
       else
       {
-        echo "<p>El numero ingresado no es mayor a 1</p>";
-        echo "<br><br>";
+        echo "<p>El numero ingresado no es mayor que 1</p>";
+        echo "<br>";
       }
-      if(is_int($_GET['numero']))
+      if(is_integer($_GET['numero']))
       {
         echo "<p>El valor ingresado es un numero entero</p>";
-        echo "<br><br>";
+        echo "<br>";
       }
       else
       {
         echo "<p>El valor ingresado no es un numero entero</p>";
-        echo "<br><br>";
+        echo "<br>";
       }
       ?>
       
